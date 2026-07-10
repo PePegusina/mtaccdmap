9dec5ff336466ef75694a9aafcefed85f81d2fb4
PePegusina <redied012@gmail.com>
2026-07-11 00:43:06 +0300

    Fix Firebase config syntax

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCO3HUyaX1fpBrkc09hfn4jec7zIXCHTYw",
-  authDomain: "mta-map-14df4.firebaseapp.com",
-  databaseURL: "https://mta-map-14df4-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "mta-map-14df4",
-  storageBucket: "mta-map-14df4.firebasestorage.app",
-  messagingSenderId: "326508782029",
-  appId: "1:326508782029:web:9e400f46a0ea4df5bd2484"
+    apiKey: "AIzaSyCO3HUyaX1fpBrkc09hfn4jec7zIXCHTYw",
+    authDomain: "mta-map-14df4.firebaseapp.com",
+    databaseURL: "https://mta-map-14df4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mta-map-14df4",
+    storageBucket: "mta-map-14df4.firebasestorage.app",
+    messagingSenderId: "326508782029",
+    appId: "1:326508782029:web:9e400f46a0ea4df5bd2484"
 };
-// Initialize Firebase
+
+// Инициализация через compat SDK (используем глобальный объект firebase)
 firebase.initializeApp(firebaseConfig);
-const app = initializeApp(firebaseConfig);
+
+// Создаем объект для работы с базой данных
+const db = firebase.database();
